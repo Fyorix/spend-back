76f033640754c5708d1d235b5219dcb277295c8a
VictorAgahi <victor@agahi.com>
2026-03-03 13:42:34 +0100

    init WS + gitignore

---
+++ ws-service/src/app.gateway.ts
@@ -0,0 +1,33 @@
+import {
+  WebSocketGateway,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  afterInit() {
+    console.log('WebSocket Gateway Initialized');
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('ping')
+  handlePing(): string {
+    return 'pong';
+  }
+}
