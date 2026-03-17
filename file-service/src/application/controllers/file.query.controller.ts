0ab311f972e0324b08099f4bedd60653e4ccd3e7
VictorAgahi <victor@agahi.com>
2026-03-17 16:14:11 +0100

    test script

--- file-service/src/application/controllers/file.query.controller.ts
+++ file-service/src/application/controllers/file.query.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/require-await */
-import { Controller, NotImplementedException } from '@nestjs/common';
+import { Controller, Logger, NotImplementedException } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import {
   FILE_SERVICE_NAME,
@@ -11,15 +11,17 @@ import {
 } from '@clement.pasteau/contracts';
 @Controller()
 export class FileQueryController {
-  constructor() {}
+  constructor() { }
 
   @GrpcMethod(FILE_SERVICE_NAME, 'GetFile')
   async getFile(_request: GetFileRequest): Promise<FileResponse> {
+    Logger.log('GET FILE NOT IMPLEMENTED');
     throw new NotImplementedException();
   }
 
   @GrpcMethod(FILE_SERVICE_NAME, 'GetUserFiles')
   async getUserFiles(_request: GetUserFilesRequest): Promise<FileListResponse> {
+    Logger.log('GET USER FILES NOT IMPLEMENTED');
     throw new NotImplementedException();
   }
 }
